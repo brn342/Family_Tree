4b4c2dc30469d125f011ce3f3139662c8b5b0891
brn342 <bruno.ocampo@correo.ucu.edu.uy>
2024-10-28 10:18:53 -0300

    Modifico program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,13 +7,21 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Person grandParent = new Person("Juan", 80);
+            Person parent1 = new Person("Ana", 55);
+            Person parent2 = new Person("Carlos", 58);
+            Person child1 = new Person("Luis", 30);
+            Person child2 = new Person("Marta", 28);
+            Person child3 = new Person("Pedro", 25);
+            Person child4 = new Person("Lucía", 22);
+            
+            Node<Person> root = new Node<Person>(grandParent);
+            Node<Person> parentNode1 = new Node<Person>(parent1);
+            Node<Person> parentNode2 = new Node<Person>(parent2);
+            Node<Person> childNode1 = new Node<Person>(child1);
+            Node<Person> childNode2 = new Node<Person>(child2);
+            Node<Person> childNode3 = new Node<Person>(child3);
+            Node<Person> childNode4 = new Node<Person>(child4);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
